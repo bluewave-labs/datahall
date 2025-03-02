74c7dc35563a09b4bec7580f97c51bdfd6d72f3e
Parwat Kunwar <parwatkunwar08@gmail.com>
2025-03-02 16:44:43 -0400

    update as per the api changes

--- src/hooks/documents/useAddDocument.ts
+++ src/hooks/documents/useAddDocument.ts
@@ -1,24 +1,24 @@
-import axios from "axios";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from 'axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const addDocument = async (formData: FormData) => {
-  const response = await axios.post("/api/documents/upload", formData);
+	const response = await axios.post('/api/documents', formData);
 
-  return response.data;
+	return response.data;
 };
 
 const useAddDocument = () => {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-  return useMutation({
-    mutationFn: addDocument,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["documents"] });
-    },
-    onError: (error) => {
-      console.error("Error adding document: ", error);
-    }
-  });
+	return useMutation({
+		mutationFn: addDocument,
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['documents'] });
+		},
+		onError: (error) => {
+			console.error('Error adding document: ', error);
+		},
+	});
 };
 
 export default useAddDocument;
--- src/hooks/documents/useDocuments.ts
+++ src/hooks/documents/useDocuments.ts
@@ -1,23 +1,22 @@
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
-import { DocumentType } from '@/shared/models/models';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { DocumentType } from '@/shared/models';
 
 interface DocumentResponse {
-  documents: DocumentType[];
+	documents: DocumentType[];
 }
 
 const fetchDocuments = async (): Promise<DocumentResponse> => {
-  const response = await axios.get('/api/documents/list');
+	const response = await axios.get('/api/documents');
 
-  return response.data;
+	return response.data;
 };
 
 const useDocuments = () => {
-  return useQuery({
-    queryKey: ["documents"],
-    queryFn: fetchDocuments
-  });
+	return useQuery({
+		queryKey: ['documents'],
+		queryFn: fetchDocuments,
+	});
 };
-
 
 export default useDocuments;
